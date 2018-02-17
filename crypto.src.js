9d9704f7649a0420ead965c535254e6f7e729971
vinceh121 <vincent.hyvert@sfr.fr>
2018-02-17 23:11:29 +0100

    Main functional commit

--- crypto.src.js
+++ crypto.src.js
@@ -3,7 +3,19 @@
  * currencies
  */
 
-Array.prototype.forEach.call(document.getElementsByClassName("crypto-button"),
-		function(node) {
-			
+var nodeList = document.getElementsByClassName("crypto-button");
+
+nodes = Array.prototype.slice.call(nodeList, 0);
+
+nodes
+		.forEach(function(node) {
+			node.style = "background-color: #e2e2e2; width: 100px; font-family: \"Courier New\"; font-size: 30px; border-radius: 5px; margin: 5px";
+			node.innerHTML = "<img src=\"https://raw.githubusercontent.com/cjdowner/cryptocurrency-icons/master/32/color/"
+					+ node.dataset.money.toLowerCase()
+					+ ".png\">"
+					+ node.dataset.money.toUpperCase();
+			node.onclick = function() {
+				alert(node.dataset.money.toUpperCase() + ": "
+						+ node.dataset.address);
+			}
 		});
