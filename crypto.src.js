40b777fa7762ae350f8a755a25cfc8482faac16c
vinceh121 <vincent.hyvert@sfr.fr>
2018-02-17 21:47:32 +0100

    Main commit

---
+++ crypto.src.js
@@ -0,0 +1,9 @@
+/**
+ * CryptoButton, an easy to use widget to create donation buttons for crypto
+ * currencies
+ */
+
+Array.prototype.forEach.call(document.getElementsByClassName("crypto-button"),
+		function(node) {
+			
+		});
