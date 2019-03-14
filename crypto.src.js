d1511cc04c8ab46f9bad33e0ef48bdb088a961dc
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2019-03-13 21:36:38 -0400

    Update crypto.src.js

--- crypto.src.js
+++ crypto.src.js
@@ -21,3 +21,29 @@ nodes
 			}
 		});
 }
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>CryptoButton</title>
+<script type="text/javascript" src="https://cdn.rawgit.com/Electro-Light/CryptoButton/55be4e60/crypto.src.js"></script>
+</head>
+<body>
+
+
+	<div class="crypto-button" data-money="L4H"
+		data-address="HFWpQU9VbEFMvEnNgdXjH6xC5zdUvKEadr"></div>
+
+	<div class="crypto-button" data-money="HTHW"
+		data-address="HF5rjaXU7s3QCKuSb6uYJvAeVa3qWC3MKm"></div>
+
+	<div class="crypto-button" data-money="TSA"
+		data-address="HQvnLwk1wvzY8jPjVtDdx8JVBnD3i6G5zk"></div>
+
+	<div class="crypto-button" data-money="MSM"
+		data-address="HEDnfLS2Mwa3vXQvbmPfQ9x1kCvingyHRe"></div>
+<script>window.onload = cryptoButton;</script>
+
+
+</body>
+</html>
